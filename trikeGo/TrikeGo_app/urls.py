bf57257d703d0fa0d4b0f89568d69e97291b0eab
GIOnyx <gregoryivanonyx.badinas@gmail.com>
2025-09-25 23:43:34 +0800

    minor fixes

--- trikeGo/TrikeGo_app/urls.py
+++ trikeGo/TrikeGo_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing"),
+    path("login.html", views.login_page, name="login_html"),
     path("login/", views.login_page, name="login"),
     path("register/", views.register_page, name="register"),
 ]
