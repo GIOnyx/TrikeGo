63b9ac7606625e347ba5be832f6a09c9ccbd51cd
GIOnyx <gregoryivanonyx.badinas@gmail.com>
2025-09-25 22:50:37 +0800

    feature(TrikeGo): created landing page UI

--- trikeGo/TrikeGo_app/views.py
+++ trikeGo/TrikeGo_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from .forms import RideForm 
 
 def landing_page(request):
-    return render(request, "TrikeGo_app/landingPage.html")
+    eta = None
+    msg = None
+
+    if request.method == "POST":
+        form = RideForm(request.POST)
+        if form.is_valid():
+            pickup = form.cleaned_data["pickup"]
+            destination = form.cleaned_data["destination"]
+
+            eta = form.get_eta()
+            msg = f"A trike will arrive in ~{eta} minutes."
+        else:
+            msg = "Please enter pickup and destination."
+    else:
+        form = RideForm()
+
+    return render(request, "TrikeGo_app/landingPage.html", {
+        "form": form,
+        "eta": eta,
+        "msg": msg
+    })
 
 def login_page(request):
     return render(request, "TrikeGo_app/login.html")
