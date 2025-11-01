f61fbc806af42a30a7a4f842543045f0c48b2892
Gregory Ivan Onyx M. Badinas <gregoryivanonyx.badinas@gmail.com>
2025-11-01 17:36:53 +0800

    Delete fare_estimation directory
    Signed-off-by: Gregory Ivan Onyx M. Badinas <gregoryivanonyx.badinas@gmail.com>

--- fare_estimation/fare.py
+++
@@ -1,9 +0,0 @@
-from .distance import calculate_distance
-
-BASE_FARE = 40     
-PER_KM_RATE = 15   
-def calculate_fare(lat1, lon1, lat2, lon2):
-    
-    distance_km = calculate_distance(lat1, lon1, lat2, lon2)
-    total_fare = BASE_FARE + (PER_KM_RATE * distance_km)
-    return round(total_fare, 2)
